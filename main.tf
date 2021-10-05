3ed8791b29b4f1788bbc000907df2bd15e5e8023
Justin OConnor <jsoconno@gmail.com>
2021-10-05 14:52:41 -0400

    removed snake as an option from the convention local block

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  convention = var.case == "kabab" ? "-${lower(var.location_acronym)}-${lower(var.workload_acronym)}-${lower(var.environment_acronym)}" : var.case == "snake" ? "_${lower(var.location_acronym)}_${lower(var.workload_acronym)}_${lower(var.environment_acronym)}" : var.case == "camel" ? "${title(var.location_acronym)}${title(var.workload_acronym)}${title(var.environment_acronym)}" : "${lower(var.location_acronym)}${lower(var.workload_acronym)}${lower(var.environment_acronym)}"
+  convention = var.case == "kabab" ? "-${lower(var.location_acronym)}-${lower(var.workload_acronym)}-${lower(var.environment_acronym)}" : var.case == "camel" ? "${title(var.location_acronym)}${title(var.workload_acronym)}${title(var.environment_acronym)}" : "${lower(var.location_acronym)}${lower(var.workload_acronym)}${lower(var.environment_acronym)}"
 }
 
 resource "null_resource" "naming_standards" {
