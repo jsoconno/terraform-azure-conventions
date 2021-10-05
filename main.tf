2c3d2a3a4ae0ad388e9fe53aa871b3df5ce651bf
Justin OConnor <jsoconno@gmail.com>
2021-10-05 14:02:39 -0400

    fix issue with logic

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 locals {
-  convention = var.force_lowercase ? "${lower(var.location_acronym)}-${lower(var.workload_acronym)}-${lower(var.environment_acronym)}" : "${var.location_acronym}-${var.workload_acronym}-${var.environment_acronym}"
+  convention = var.lowercase ? "${lower(var.location_acronym)}-${lower(var.workload_acronym)}-${lower(var.environment_acronym)}" : "${var.location_acronym}-${var.workload_acronym}-${var.environment_acronym}"
 }
 
 resource "null_resource" "naming_standards" {
